d55dce194d22623a0a553fb226ebe78eedc497d1
banerbaner <53179710+banerbaner@users.noreply.github.com>
2019-08-02 17:09:38 +0530

    Add files via upload

---
+++ Boiler Plate/sketch.js
@@ -0,0 +1,34 @@
+var fixedRect, movingRect;
+
+function setup() {
+  fixedRect = createSprite(200,200,50,10);
+  fixedRect.shapeColor = "blue";
+
+  movingRect = createSprite(100,100,80,50);
+  movingRect.shapeColor = "blue";
+
+  createCanvas(800,400);
+}
+
+function draw() {
+  background(0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && fixedRect.x - movingRect.x < 
+    movingRect.width/2 + fixedRect.width/2 && 
+    movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 && fixedRect.y - movingRect.y < 
+    movingRect.height/2 + fixedRect.height/2){
+  
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "orange";
+  }
+  else{
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "blue";
+  }
+
+
+  
+  drawSprites();
+}
